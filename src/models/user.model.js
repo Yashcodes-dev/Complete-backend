8a41744d5672268f5c14425271d58ec7651f91d4
Yash <yashrajchandra9th13@gmail.com>
2026-08-27 00:01:23 +0530

    not so imp

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -66,10 +66,11 @@ userSchema.methods.generateAccessToken = function(){
    return jwt.sign(
         {
             _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullname: this.fullname,
         },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
     )
 }
 userSchema.methods.generateRefreshToken = function(){}
